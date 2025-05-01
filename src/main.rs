e58bf55e145f6bcfcfaddd05f85c25294373949c
Gonza0086 <hgonzalo2000@gmail.com>
2025-05-01 16:04:56 -0300

    saving password into file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::fs::OpenOptions;
+use std::io::Write;
+
 use rand::random_range;
 
 fn generate_random_char() -> String {
     let safe_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#%^&*-_=+";
-    let random_char = safe_chars.chars().nth(random_range(0..76)).unwrap();
+    let random_char = safe_chars.chars().nth(random_range(0..72)).unwrap();
 
     return String::from(random_char);
 }
@@ -17,6 +20,21 @@ fn generate_password() -> String {
     return password;
 }
 
+fn write_password_into_file(password: &str) -> std::io::Result<()> {
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("keeper.txt")?;
+
+    writeln!(file, "{}", password)?;
+
+    Ok(())
+}
+
 fn main() {
-    println!("Password: {}", generate_password());
+    let password = generate_password();
+    if let Err(e) = write_password_into_file(&password) {
+        eprintln!("Could not write file: {}", e);
+    }
+    println!("Password: {}", password);
 }

052282132638909dd4ea9ccc73e5165ae6dcf309
Gonza0086 <hgonzalo2000@gmail.com>
2025-05-02 19:09:12 -0300

    modular secrets_keeper and error handling for args

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,17 @@
+use password_generator::PasswordGenerator;
+use secrets_manager::SecretsKeeper;
 use std::env;
-use std::fs::{self, File};
-use std::io::{BufRead, BufReader};
 
 mod password_generator;
-
-use password_generator::PasswordGenerator;
-
-struct SecretsKeeper {
-    path: String,
-}
-
-impl SecretsKeeper {
-    pub fn new(path: &str) -> SecretsKeeper {
-        SecretsKeeper {
-            path: path.to_string(),
-        }
-    }
-
-    pub fn add(&self, app_name: String, password: &str) {
-        let lines = self.read_file();
-
-        let mut new_password_added = false;
-        let mut new_content = String::new();
-
-        for line in lines {
-            new_content += &format!("{}\n", line);
-
-            if line == app_name {
-                new_content += &format!("{}\n", password);
-                new_password_added = true;
-            }
-        }
-
-        if !new_password_added {
-            new_content += &format!("{}\n{}\n", app_name, password);
-        }
-
-        self.write_file(new_content);
-    }
-
-    fn read_file(&self) -> Vec<String> {
-        let file = File::open(&self.path).expect("Error reading file!");
-        let file_reader = BufReader::new(file);
-        let lines = file_reader
-            .lines()
-            .collect::<std::io::Result<Vec<String>>>()
-            .expect("Error parsing lines!");
-
-        return lines;
-    }
-
-    fn write_file(&self, new_content: String) {
-        fs::write(&self.path, new_content.as_bytes()).expect("Error writting file!");
-    }
-}
+mod secrets_manager;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        panic!("More options are required: --verb app_name");
+    }
+
     let verb = &args[1];
     let app_name = &args[2];
     let secrets_keeper = SecretsKeeper::new("keeper.txt");
---
+++ src/secrets_manager/mod.rs
@@ -0,0 +1,51 @@
+use std::fs::{self, File};
+use std::io::{BufRead, BufReader};
+
+pub struct SecretsKeeper {
+    path: String,
+}
+
+impl SecretsKeeper {
+    pub fn new(path: &str) -> SecretsKeeper {
+        SecretsKeeper {
+            path: path.to_string(),
+        }
+    }
+
+    pub fn add(&self, app_name: String, password: &str) {
+        let lines = self.read_file();
+
+        let mut new_password_added = false;
+        let mut new_content = String::new();
+
+        for line in lines {
+            new_content += &format!("{}\n", line);
+
+            if line == app_name {
+                new_content += &format!("{}\n", password);
+                new_password_added = true;
+            }
+        }
+
+        if !new_password_added {
+            new_content += &format!("{}\n{}\n", app_name, password);
+        }
+
+        self.write_file(new_content);
+    }
+
+    fn read_file(&self) -> Vec<String> {
+        let file = File::open(&self.path).expect("Error reading file!");
+        let file_reader = BufReader::new(file);
+        let lines = file_reader
+            .lines()
+            .collect::<std::io::Result<Vec<String>>>()
+            .expect("Error parsing lines!");
+
+        return lines;
+    }
+
+    fn write_file(&self, new_content: String) {
+        fs::write(&self.path, new_content.as_bytes()).expect("Error writting file!");
+    }
+}
